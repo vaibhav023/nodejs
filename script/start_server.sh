0a103e752985ade98fe7e4a036b2b2ab6f9e0180
vaibhav023 <91330995+vaibhav023@users.noreply.github.com>
2021-10-28 19:12:36 +0530

    Update start_server.sh

--- script/start_server.sh
+++ script/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/back/node-express-realworld-example-app
+cd /home/ubuntu/node-express-realworld-example-app
 sudo npm install -d 
 sudo npm install pm2 -g
 sudo pm2 start app.js
